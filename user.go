aad3fe56b83de34ede3baa3e846f283c38717a65
aquilax <aquilax@gmail.com>
2013-01-13 18:46:29 +0000

    Added schema

--- user.go
+++ user.go
@@ -4,15 +4,16 @@ import (
 	"time"
 	"fmt"
 	"net/http"
+	"github.com/gorilla/schema"
 )
 
 type User struct {
 	Id int `PK`
-	Email string
-	Password string
-	Status int
-	Last_login time.Time
-	Created time.Time
+	Email string `schema:"email"`
+	Password string `schema:"password"`
+	Status int `schema:"-"`
+	Last_login time.Time `schema:"-"`
+	Created time.Time `schema:"-"`
 }
 
 func NewUser () *User {
@@ -20,13 +21,24 @@ func NewUser () *User {
 }
 
 func (u *User) Init () {
+	http.HandleFunc("/auth/register", func(w http.ResponseWriter, r *http.Request) {
+		_ = u.Register(r)
+	})
+
 	http.HandleFunc("/auth/login", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello")
 		u.Email = "test@test.com"
 		u.Save()
-	});
+	})
 }
 
 func (u *User) Save () {
 	game.db.orm.Save(u)
+}
+
+func (u *User) Register (r *http.Request) int {
+	decoder := schema.NewDecoder();
+	decoder.Decode(u, r.Form)
+	fmt.Print(u);
+	return 0
 }
